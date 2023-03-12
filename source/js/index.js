ce632a06071cf884ea090ff122a3d52b4842fd6f
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-03-12 22:45:47 +0300

    Improve markup of the 'page-header' block

--- source/js/index.js
+++ source/js/index.js
@@ -1,10 +1,10 @@
 import Nav from './nav.js';
 
 const work = () => {
-  const navElem = document.querySelector(`.main-nav`);
+  const navElem = document.querySelector(`.page-header__nav`);
 
   if (navElem !== null) {
-    const nav = new Nav(navElem, `main-nav__toggle`, `main-nav__list`);
+    const nav = new Nav(navElem, `toggle-nav-button`, `site-list`);
     nav.init();
   }
 };
